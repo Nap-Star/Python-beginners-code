25a73fc1ff51c56315a3a76a9917773aeeb43484
Nap-Star <nandika.prasad.14@gmail.com>
2025-07-19 18:34:29 +0100

    Add files via upload

---
+++ PrimeNumber.py
@@ -0,0 +1,15 @@
+#Find out if it's a prime number
+x=int(input("Enter a number"))
+
+if x%2 == 0:
+  print("This is not a prime number") 
+elif x%3 == 0:
+  print("This is not a prime number") 
+elif x%5 == 0:
+  print("This is not a prime number") 
+elif x%7 == 0:
+  print("This is not a prime number") 
+else:
+  print("This is a prime number") 
+
+
